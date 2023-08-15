a337ddf4410f57eec96322e2404850510dcf23a8
Ratik Vig <ratikvig@gmail.com>
2023-08-15 10:49:15 +0530

    added new app icons and resume

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -1,10 +1,12 @@
 import PhoneApps from "../PhoneApps/PhoneApps"
 import PhoneDock from "../PhoneDock/PhoneDock"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPhone, faSchool, faUser } from "@fortawesome/free-solid-svg-icons"
+import { faBriefcase, faCode, faFile, faLaptopCode, faPenRuler, faPencilRuler, faPhone, faSchool, faUser } from "@fortawesome/free-solid-svg-icons"
 import './HomeScreen.css'
 import { useEffect, useRef } from "react"
 import PhoneWidget from "../PhoneWidget/PhoneWidget"
+import { FaPencilRuler } from "react-icons/fa"
+import { faFilePdf } from "@fortawesome/free-regular-svg-icons"
 
 const HomeScreen = ({setApp}) => {
    
@@ -29,12 +31,19 @@ const HomeScreen = ({setApp}) => {
                     }}
                 />
                 <PhoneApps 
-                    icon={<FontAwesomeIcon icon={faPhone} style={{color: 'white', fontSize: 24}}/>}
-                    label={"Phone"}
+                    icon={<FontAwesomeIcon icon={faBriefcase} style={{color: '#fff', fontSize: 24}}/>}
+                    iStyle={{background: `linear-gradient(0deg, rgba(121,15,161,1) 0%, rgba(201,125,224,1) 34%)`}}
+                    label={"Work Exp."}
                 />
                 <PhoneApps 
-                    icon={<FontAwesomeIcon icon={faPhone} style={{color: 'white', fontSize: 24}}/>}
-                    label={"Phone"}
+                    icon={<FontAwesomeIcon icon={faLaptopCode} style={{color: '#303030', fontSize: 24}}/>}
+                    label={"Projects"}
+                    iStyle={{background: '#fff600'}}
+                />
+                <PhoneApps 
+                    icon={<FontAwesomeIcon icon={faFilePdf} style={{color: '#2c3e50', fontSize: 24}}/>}
+                    label={"Resume"}
+                    handleClick={() => window.open("https://drive.google.com/file/d/1RjyzEFXOcniFD58ft1yKpR1umwD0sMMP/view?usp=sharing")}
                 />
             </div>
         </div>
