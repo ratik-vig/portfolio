6759576045262958138c133364b1c5728ffca830
Ratik Vig <ratikvig@gmail.com>
2023-08-15 11:56:30 +0530

    completed slider and education page

--- src/components/CardSlider/CardSlider.jsx
+++ src/components/CardSlider/CardSlider.jsx
@@ -1,24 +1,46 @@
 import './CardSlider.css'
 import nyu from '../../nyu.png'
+import bennett from '../../bennettLogo.gif'
+import Lists from '../Lists/Lists'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import {useState} from 'react'
 
-const Card = () => {
+const Card = ({item}) => {
     return (
         <div style={{width: '100%'}}>
-            <img src={nyu} style={{width: '60%', marginLeft: '50%', transform: `translateX(-50%)`, borderRadius: 10}} />
-            <h2 style={{fontSize: 16, color: '#303030', textAlign: 'center'}}>New York University</h2>
-            <p style={{fontSize: 14, color: '#303030', textAlign: 'center'}}>Master's in Computer Engineering</p>
-            <p style={{fontSize: 12, color: '#303030', textAlign: 'center'}}>May 2024</p>
-            <p style={{fontSize: 14, color: '#303030', textAlign: 'center', fontWeight: 'bold'}}>GPA 3.89/4.0</p>
+            <img src={item.id === 1 ? nyu : bennett} style={{width: '60%', marginLeft: '50%', transform: `translateX(-50%)`, borderRadius: 10, backgroundColor: '#303030'}} />
+            <h2 style={{fontSize: 16, color: '#303030', textAlign: 'center'}}>{item.name}</h2>
+            <p style={{fontSize: 14, color: '#303030', textAlign: 'center'}}>{item.degree}</p>
+            <p style={{fontSize: 12, color: '#303030', textAlign: 'center'}}>{item.graduation}</p>
+            <p style={{fontSize: 14, color: '#303030', textAlign: 'center', fontWeight: 'bold'}}>{`GPA ${item.gpa}`}</p>
+            <Lists listItems={item.courses} />
+
         </div>
     )
 }
 
-const CardSlider = () => {
+const CardSlider = ({data}) => {
+    const [pageNo, setPage] = useState(0)
+    const handlePageChange = direction => {
+        setPage(pageNo + direction)
+    }
+
     return(
-        <div style={{width: '80%'}}>
+        <div style={{ width: '80%', overflow: 'hidden'}}>
+            <div style={{width: '200%', display: 'flex', transform: `translateX(${pageNo === 0 ? '0%' : '-50%'})`, transition: 'all 0.5s ease'}}>
+                {data.map(item => {
+                    return(
+                        <Card item={item} />
+                    )
+                })}
+            </div>
             
-            <Card />
-            
+            <div className='slider-control-wrapper'>
+                <FontAwesomeIcon icon={faChevronLeft} className={`chevron-button ${pageNo === 0 && 'button-disable'}`} onClick={() => handlePageChange(-1)}/>
+                <p className='page-text'> {`${pageNo+1} of 2`}</p>
+                <FontAwesomeIcon icon={faChevronRight} className={`chevron-button ${pageNo === 1 && 'button-disable'}`} onClick={() => handlePageChange(1)}/>
+            </div>
         </div>
     )
 }
--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -11,12 +11,42 @@ const Education = ({setApp}) => {
         "Realtime Embedded Systems",
         "Computer Systems Architecture"
     ]
+    const data = [
+        {
+            id: 1,
+            name: 'New York University',
+            degree: `Master's in Computer Engineering`,
+            graduation: 'May 2024',
+            gpa: '3.89/4.0',
+            courses: [
+                "Data Structures and Algorithms",
+                "Database Design and Principles",
+                "Internet Architecture and Protocols",
+                "Realtime Embedded Systems",
+                "Computer Systems Architecture"
+            ]
+        },
+        {
+            id: 2,
+            name: 'Bennett University',
+            degree: `Bachelor's in Computer Science`,
+            graduation: 'June 2020',
+            gpa: '8.1/10.0',
+            courses: [
+                "Data Structures and Algorithms",
+                "Database Design and Principles",
+                "Internet Architecture and Protocols",
+                "Realtime Embedded Systems",
+                "Computer Systems Architecture"
+            ]
+        }
+    ]
     return(
         <div style={{ height: '100%', overflow: 'scroll', backgroundColor: '#f5f5f7', animation: 'openApp 0.2s'}}>
             <AppHeader setApp={setApp} appTitle={"Education"}/>
             <div className='details-container'>
-                <CardSlider />
-                <Lists listItems={coursesMasters} />
+                <CardSlider data={data}/>
+                {/* <Lists listItems={coursesMasters} /> */}
             </div>
         </div>
     )
